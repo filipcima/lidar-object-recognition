982acef511b63d1bd5188054f71b5d3ecc824c96
Filip Cima <filip.cima.st@vsb.cz>
2019-03-24 12:39:49 +0100

    fix erorrs

--- src/SqueezeSeg.py
+++ src/SqueezeSeg.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import, print_function
 
 import argparse
 import os
@@ -68,8 +68,8 @@ class SqueezeSeg(object):
         :param name: module name
         :return: [N, H, 2*W, C]
         """
-        C = input_tensor.shape[-1]
-
+        C = int(input_tensor.shape[-1])
+        
         conv_1 = tf.layers.conv2d(
             inputs=input_tensor,
             filters=C/2,
@@ -178,17 +178,19 @@ class SqueezeSeg(object):
         fire_deconv_13 = self.fire_deconv_module(skip_conv_1a_deconv_12, 'fire_deconv_13')
 
         skip_conv_1b_deconv_13 = tf.concat([input_layer, fire_deconv_13], -1)
+       
+        print(skip_conv_1b_deconv_13)
 
         conv_14 = tf.layers.conv2d(
             inputs=skip_conv_1b_deconv_13,
             filters=self.num_classes,
             kernel_size=[1, 1],
             strides=[1, 1],
-            activation=tf.nn.relu, padding='same', name='conv14'
+            activation=tf.nn.relu, padding='same', name='conv_14'
         )
-
+        
         logits = conv_14
-
+        
         predictions = {
             'classes': tf.argmax(input=logits, axis=3),
             'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
@@ -196,10 +198,17 @@ class SqueezeSeg(object):
 
         if mode == tf.estimator.ModeKeys.PREDICT:
             return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+        
+        print(labels)
 
         one_hot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=self.num_classes)
         weights = tf.constant([0.5, 0.3, 0.2])
         class_weights = tf.multiply(one_hot_labels, weights)
+
+        print('One hot labels:',one_hot_labels)
+        print('Logits:', logits)
+        print('Class weights', class_weights)
+
         cross_entropy = tf.multiply(
             class_weights,
             tf.losses.softmax_cross_entropy(
@@ -237,7 +246,8 @@ def get_data(mode='train'):
     for path in data_set_paths:
         if current_count % 200 == 0:
             print('Processed: {} %'.format(current_count * 1.0 / data_count * 100.0))
-        input_tensors.append(np.load(path).astype(np.float32))
+        if current_count % 4:
+            input_tensors.append(np.load(path).astype(np.float32))
         current_count += 1
 
     print('{}ing data loaded.'.format(mode))
@@ -253,17 +263,28 @@ def main():
     args = parser.parse_args()
 
     squeeze_seg = SqueezeSeg(batch_size=args.batch_size, epochs=args.epochs, log_info=args.verbose)
-    classifier = tf.estimator.Estimator(model_fn=squeeze_seg.squeeze_seg_fn, model_dir='./model/')
+    # config = tf.ConfigProto(allow_soft_placement=True)
+    # config.gpu_options.allow_growth=True
+    # run_config = tf.estimator.RunConfig(session_config=config)
+    classifier = tf.estimator.Estimator(
+        model_fn=squeeze_seg.squeeze_seg_fn,
+        model_dir='./model/',
+        # config=run_config
+    )
 
     whole_train_data = np.array(get_data('train')).astype(np.float32)
-    train_data = whole_train_data[:, :, :5]
-    train_labels = whole_train_data[:, :, 5]
+    print('whole_train', whole_train_data.shape)
+    train_data = whole_train_data[:, :, :, :5]
+    train_labels = whole_train_data[:, :, :, 5]
     whole_eval_data = np.array(get_data('test')).astype(np.float32)
-    eval_data = whole_eval_data[:, :, :5]
-    eval_labels = whole_eval_data[:, :, 5]
+    print('whole_eval', whole_eval_data.shape)
+    eval_data = whole_eval_data[:, :, :, :5]
+    eval_labels = whole_eval_data[:, :, :, 5]
 
     tensors_to_log = {"probabilities": "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=10)
+
+    print('train labels',train_labels.shape)
 
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": train_data},
